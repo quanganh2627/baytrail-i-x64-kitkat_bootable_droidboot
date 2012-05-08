8bd44dd9249865396ccbb7cf80c300eddbc6c8b9
Matt Gumbel <matthew.k.gumbel@intel.com>
2012-05-08 10:59:48 -0700

    Listen for fastboot on both USB and TCP
    BZ: 110558
    
    For devices where we want to use fastboot, but no client USB port is
    available, we listen on TCP.
    
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>
    (cherry picked from commit 1a3d68af4f465f192f5a27a5fe6afdd26691fc9c)
    
    Change-Id: Iad2679cedeb929cd0fe1179c891c76ee8a04edac
    Reviewed-on: http://android.intel.com:8080/108011
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Muller, Francois-nicolas <francois-nicolas.muller@intel.com>
    Reviewed-by: Falempe, Jocelyn <jocelyn.falempe@intel.com>
    Tested-by: Falempe, Jocelyn <jocelyn.falempe@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -31,7 +31,7 @@ endif
 
 LOCAL_MODULE := droidboot
 LOCAL_MODULE_TAGS := eng
-LOCAL_SHARED_LIBRARIES := liblog libext4_utils libz
+LOCAL_SHARED_LIBRARIES := liblog libext4_utils libz libcutils
 LOCAL_STATIC_LIBRARIES += libmtdutils libpng libpixelflinger_static libc libcutils libmtdutils
 LOCAL_STATIC_LIBRARIES += $(TARGET_DROIDBOOT_LIBS) $(TARGET_DROIDBOOT_EXTRA_LIBS) libminzip
 
