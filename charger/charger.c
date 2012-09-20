c74da1a312fbce526927d84451938668cf500558
Xiaokang Qin <xiaokang.qin@intel.com>
2012-09-20 15:54:21 +0800

    Always include <sys/socket.h> before <linux/netlink.h>.
    BZ: 50889
    
    Newer kernel headers files aren't stand alone. (This has long been true
    of glibc's copy, but we only recently updated external/kernel-headers.)
    
    Change-Id: I1194a10c714cf2adc4a61b4594364dbaa0f064c6
    Signed-off-by: Xiaokang Qin <xiaokang.qin@intel.com>
    Reviewed-on: http://android.intel.com:8080/67169
    Reviewed-by: Zhang, Hongyu <hongyu.zhang@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- charger/charger.c
+++ charger/charger.c
@@ -20,6 +20,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
+#include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/rtc.h>
 #include <stdbool.h>
@@ -27,7 +28,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/poll.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/un.h>
