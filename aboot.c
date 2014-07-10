12f82ceb60ba9357c84b103a301f885abbfef69f
Jeremy Compostella <jeremy.compostella@intel.com>
2014-07-10 12:29:46 +0200

    flash: support filesystem sparse file
    fastboot format command sends a sparse ext4 file system image.  This
    patch add the support of such an image in the context of the flash
    command.
    
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-278
    Change-Id: Ib18c727864f3181469ed3303c906d2fefc6b075f
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://android.intel.com:443/274831

--- aboot.c
+++ aboot.c
@@ -340,7 +340,7 @@ static void cmd_flash(const char *part_name, void *data, unsigned sz)
 	char path[FILE_NAME_SIZ];
 	int ret = -1;
 	flash_func cb;
-	Volume *v;
+	Volume *v = NULL;
 
 	ui_print("FLASH %s...\n", part_name);
 
@@ -360,7 +360,10 @@ static void cmd_flash(const char *part_name, void *data, unsigned sz)
 			}
 			snprintf(path, FILE_NAME_SIZ, "%s", v->device);
 		}
-		if ((ret = named_file_write(path, data, sz)) < 0) {
+		if (v && ((sparse_header_t*)data)->magic == SPARSE_HEADER_MAGIC) {
+			pr_info("sparse image data detected flash sparse package\n");
+			ret = write_ext4_sparse(v->device, data, sz);
+		} else if ((ret = named_file_write(path, data, sz)) < 0) {
 			pr_error("Can't write data to target %s!\n", path);
 			goto out;
 		}
